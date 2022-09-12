79c24da376b7c674a5af0ed190ec67701f29201c
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2022-09-12 19:13:42 -0400

    Changed degrees to imperial

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -5,7 +5,7 @@ export default function WeatherTemperature(props) {
     <div className="WeatherTemperature">
       <div className="WeatherTemperature-container">
       <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">°C</span>
+      <span className="unit">°F</span>
       </div>
     </div>
   );
