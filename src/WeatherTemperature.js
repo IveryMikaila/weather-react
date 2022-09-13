fcda5f51da9e9393d1966d458b9ef9445aa6c409
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2022-09-12 20:20:25 -0400

    Added Fahrenheit and Celsius

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function WeatherTemperature(props) {
+const [unit, setUnit] = useState("fahrenheit");
+function convertToC (event){
+  event.preventDefault();
+  setUnit("celcius")
+}
+
+function convertToF (event){
+  event.preventDefault();
+  setUnit("fahrenheit");
+}
+
+if(unit === "fahrenheit"){
   return (
     <div className="WeatherTemperature">
       <div className="WeatherTemperature-container">
       <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">°F</span>
+      <span className="unit" >°F |{" "}
+      <a href="/" onClick={convertToC} >°C</a></span>
       </div>
     </div>
   );
+}
+else{
+  let celsius = (props.celsius - 32) * 5/9 ;
+return (
+  <div className="WeatherTemperature">
+    <div className="WeatherTemperature-container">
+    <span className="temperature">{Math.round(celsius)}</span>
+    <span className="unit" ><a href="/" onClick={convertToF} >°F |</a>{" "}
+   °C</span>
+    </div>
+  </div>
+);
+}
 }
